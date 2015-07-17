bb1f1c273f289333be3198f41fd27ad4c1f6c867
maxmurder <rusty657@shaw.ca>
2015-07-17 14:16:44 -0600

    added rcurses-manager.cpp

---
+++ src/rcurses-manager.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+#include <ncurses.h>
+using namespace std;
+
+namespace rcurses-manager 
+{
+        class screen-manager
+        {
+                vector<SCREEN *> screens;
+                
+                public:
+                int screen-manager(){
+                }
+                
+                void ~screen-manager(){
+                        for (i=0;i<screens.size();i++)
+                        {
+                                RemoveScreen(i);
+                        }
+                }
+                
+                SCREEN * AddScreen() {
+                        t = newterm();
+                        screens.push_back(t);
+                        return t;
+                }
+                
+                void RemoveScreen(SCREEN * t){
+                        set_term(t);
+                        endwin();
+                        screens.erase(find(screens.begin(), screens.end(), t);
+                }
+                
+                void RemoveScreen(int t){
+                        set_term(screens[t]);
+                        endwin();
+                        screens.erase(t);
+                }
+                
+                vector<SCREEN *> GetScreens(){
+                        return screens;
+                }
+        }
+}
