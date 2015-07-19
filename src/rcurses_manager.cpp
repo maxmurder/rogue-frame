7168a5911794693001040a8ab95936fac913c60b
maxmurder <rusty657@shaw.ca>
2015-07-19 01:28:28 -0600

    added element_manager code

--- src/rcurses_manager.cpp
+++ src/rcurses_manager.cpp
@@ -5,9 +5,7 @@
 using namespace std;
 using namespace rcurses_manager;
 
-screen_manager::screen_manager(){
-}
-
+screen_manager::screen_manager(){}
 screen_manager::~screen_manager(){
     for (auto& s: screens){
         RemoveScreen(s.first);
@@ -16,9 +14,11 @@ screen_manager::~screen_manager(){
 
 int screen_manager::AddScreen() {
     //create terminal and add to the screen list
+    
+    static int screenCount;
     int i = screenCount;
-    SCREEN *t = newterm(NULL,stdout,stdin);
-    screens[i] = t;
+    SCREEN *s = newterm(NULL,stdout,stdin);
+    screens[i] = s;
     screenCount++;
     
     //init ncurses
@@ -31,10 +31,37 @@ int screen_manager::AddScreen() {
     return i;
 }
 
-void screen_manager::RemoveScreen(int t){
-    set_term(screens[t]);
+void screen_manager::RemoveScreen(int s){
+    set_term(screens[s]);
     endwin();
-    screens.erase(t);
+    screens.erase(s);
+}
+
+SCREEN * GetScreen(int screen_index){
+    return screens[screen_index];    
+}
+
+/*element_manager::~element_manager(){
+    for (auto& w: windows){
+        int i = w.first;
+        RemoveWindow(i);
+    }
+}*/
+
+int element_manager::AddWindow(int screen_index, int height, int width, int startx, int starty){
+    static int windowCount;
+    int i = windowCount;
+    set_term(screens[screen_index]);
+    WINDOW *w = newwin(height,width,startx,starty);
+    windows[i] = w;
+    wrefresh(w);    
+    return i;
+}
+void element_manager::RemoveWindow(int screen_index, int window_index){
+    set_term(screens[screen_index]);
+    werase(windows[window_index]);
+    delwin(windows[window_index]);
+    windows.erase(window_index);
 }
 
 
--- src/rcurses_manager.h
+++ src/rcurses_manager.h
@@ -6,16 +6,27 @@
 
 namespace rcurses_manager
 {
+
+    //std::map<int, SCREEN *> screens; //map of current screens
+
     class screen_manager
     {   
         public:
+            std::map<int, SCREEN *> screens; //map of current screens
             int AddScreen(); // add new terminal screen
-            void RemoveScreen(int t); //remove terminal screen by id
+            void RemoveScreen(int screen_index); //remove terminal screen by index
+            SCREEN * GetScreen(int screen_index);//get screen pointer
             screen_manager();
             ~screen_manager();
-        private:
-            std::map<int, SCREEN *> screens; //list of current screens
-            int screenCount;
+    };
+    class element_manager
+    {
+        public:
+            int AddWindow(int screen_index, int height, int width, int startx, int starty); //add a new window to a screen
+            void RemoveWindow(int screen_index, int window_index); //remove a window
+            //void RefreshWindow(int window_index);
+            //~element_manager();
+            std::map<int, WINDOW *> windows;
     };
 };
 
