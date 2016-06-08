5378695e772727ee79074e95780a2e78f4d3545c
maxmurder <rusty657@shaw.ca>
2016-06-07 23:25:53 -0700

    Added missing files

--- data/lua/test.lua
+++ data/lua/test.lua
@@ -1,6 +1,15 @@
-animation={0x263A, 0x263B}
-framerate=1
-test={
-  string='test',
-  position={x=128,y=64},
-      }
+test = {
+    position = { 
+        x = 256, 
+        y = 272
+    },
+    string = "Hello World!"
+}
+testArray = { 0, 0x263A, 2, 3, 4, 5}
+testStrArray = {"H","E","LL","0"}
+testFarray = {0,0.1,0.2,0.3,0.4,0.5}
+
+framerate = 5
+
+animation = {0x263A,0x263B}
+position = {256,286}
